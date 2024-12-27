c2ef80bc9bfa94e8c3d66770066c6713c3f745e3
UniDroIPL <2221264@my.ipleiria.pt>
2024-12-27 00:30:36 +0000

    Adição de Favoritos
    - Adição das imagens dos favoritos.
    - Implementação da verificação dos favoritos quando o card é criado.
    -Implementação da função de adicionar e remover favoritos.

--- assets/js/paises.js
+++ assets/js/paises.js
@@ -2,35 +2,63 @@
 
 import {apiCall} from "./api.js";
 
+
+
 $(document).ready(function(){
     apiCall("https://restcountries.com/v3.1/all?fields=name,cca3,flags,capital", displayPaises, 5)
 })
 
 
 
+$(".pais-display").on("click", ".btn-icon-fav", function(){
+    let id = $(this).attr("id");
+    localStorage.setItem(id, id)
+    $(this).removeClass('btn-icon-fav').addClass('btn-icon-defav');
+    $(this).find('img').remove()
+    $(this).append('<img src="assets/img/fav-full.svg" alt="icon favorito" class="img-fluid"></img>');
+  });
+
+$(".pais-display").on("click", ".btn-icon-defav", function(){
+    console.log("botão defav clicado");
+    let id = $(this).attr("id");
+    
+    localStorage.removeItem(id, id)
+    $(this).removeClass('btn-icon-defav').addClass('btn-icon-fav');
+    $(this).find('img').remove()
+    $(this).append('<img src="assets/img/fav-empty.svg" alt="icon favorito" class="img-fluid"></img>');
+  });
+
 function displayPaises(dataPaises){
     $.each(dataPaises, function(index,dataPais){
         
         console.log(dataPais.flags['png']);
-        let fotoPais = dataPais.flags['svg'];
-        
+        let fotoPais = dataPais.flags['png'];
+        let id = dataPais.cca3;
+        let favIcon = "assets/img/fav-empty.svg";
+        let btn = "btn-icon-fav";
+
+
+        if (isPaisFavorite(id)){
+            favIcon = "assets/img/fav-full.svg";
+            btn = "btn-icon-defav"
+        }
 
         let card = 
         `        <div class = "col-md-3">
                     <div class="card">
-                        <img class="card-img-top" src="${fotoPais}" alt="Foto do Pais">
+                        <img class="card-img-top custom-img" src="${fotoPais}" alt="Foto do Pais">
                         <div class="card-body">
                             <div class="row">  
                                 <div class="col-9">
                                     <h5 class="card-title">Titulo card</h5>
                                 </div>
                                 <div class="col-3">
-                                    <button class="btn btn-primary" id="xopa">
-                                      mete imagem aqui
+                                    <button class="${btn}" id="${id}">
+                                        <img src="${favIcon}" class="img-fluid"></img>
                                     </button>
                                 </div>
                             </div>
-                            <p class="card-text">Alguma informação relativa a este pequeno patudo, lindo e fofo!</p>
+                            <p class="card-text">Capital: Capital X</p>
                             <div class="text-center">
                                 <button type="button" class="btn btn-primary btn-ver-mais" id="xopa">Ver mais</button>
                             </div>
@@ -42,4 +70,11 @@ function displayPaises(dataPaises){
         $(".pais-display").append(card);
         
     })
+
+    function isPaisFavorite(id){
+        if(id == localStorage.getItem(id)){
+          return true;
+        }
+        return false;
+      }
 }
